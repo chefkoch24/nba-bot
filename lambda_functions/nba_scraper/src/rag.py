a58c097bda6ccc6014117df00f54467f5c920857
chefkoch24 <kuennecke-felix@web.de>
2024-06-06 22:31:34 +0200

    update from gpt-3.5 to gpt-4o

--- lambda_functions/nba_scraper/src/rag.py
+++ lambda_functions/nba_scraper/src/rag.py
@@ -21,7 +21,7 @@ class RAG:
                 \n {d['game_recap']}"""
                 # request model
                 response = self.client.chat.completions.create(
-                    model="gpt-3.5-turbo",
+                    model='gpt-4o',
                     messages=[
                         {"role": "user", "content": prompt},
                             ]
